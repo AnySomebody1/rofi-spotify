41b69db621d520099e09143840ad5c4b7048b2cd
Any Somebody <anysomebody@gmx.de>
2020-04-08 08:30:43 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rofi_spotify',
+    version='0.0.1',
+    install_requires=['python-rofi', 'spotipy'],
+    packages=['rofi', 'rofi-spotify'],
+    url='https://github.com/AnySomebody1/rofi-spotify',
+    license='GPLv3+',
+    author='Any Somebody',
+    author_email='anysomebody@gmx.de',
+    description='A Rofi menu for interacting with Spotify.',
+    scripts=['bin/rofi-spotify']
+)
