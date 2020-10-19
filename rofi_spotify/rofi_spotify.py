55c4c6dee06ed549408a25c01032a4c17d0bc9c3
Any Somebody <anysomebody@gmx.de>
2020-10-19 22:29:59 +0200

    Fixed get tracks from playlist, replaced deprecated authentication

--- rofi_spotify/rofi_spotify.py
+++ rofi_spotify/rofi_spotify.py
@@ -13,8 +13,9 @@ import time
 
 import appdirs
 import spotipy
-import spotipy.util as util
+# import spotipy.util as util
 from rofi import Rofi
+
 
 def load_config():
     config_dir = appdirs.user_config_dir('rofi-spotify')
@@ -27,14 +28,17 @@ def load_config():
         print("No config file found, let's create one...")
         print(textwrap.wrap("Please visit https://developer.spotify.com/dashboard/applications and click on"
                             " \"Create a client id\".", width=70))
-        print(textwrap.wrap("Enter arbitrary name and description and select appropriate options in the next steps.", width=70))
-        print(textwrap.wrap("Click on \"Edit Settings\" and add an arbitrary Redirect URI, for instance http://localhost:8080 by entering"
-              " it, clicking on \"Add\" and finally on \"Save Settings\" below.", width=70))
+        print(textwrap.wrap("Enter arbitrary name and description and select appropriate options in the next steps.",
+                            width=70))
+        print(textwrap.wrap("Click on \"Edit Settings\" and add an arbitrary Redirect URI, for instance "
+                            "http://localhost:8080 by entering"
+                            " it, clicking on \"Add\" and finally on \"Save Settings\" below.", width=70))
 
         config = configparser.ConfigParser()
         config['spotipy'] = {}
         config['spotipy']['client_id'] = str(input("Please enter the Client ID you received after the previous step: "))
-        config['spotipy']['client_secret'] = str(input("Please enter the Client Secret you received after the previous step: "))
+        config['spotipy']['client_secret'] = str(input("Please enter the Client Secret you received after the previous "
+                                                       "step: "))
         config['spotipy']['redirect_uri'] = str(input("Please enter the Redirect URI you added: "))
 
         config['spotify'] = {}
@@ -46,8 +50,10 @@ def load_config():
             config.write(configfile)
     return config, config_dir
 
+
 def getPlaylists(sp, onlyEditable=True):
     return sp.current_user_playlists(limit=50)
+
 
 def getCurrentTrackID(sp):
     current_playback = sp.current_playback()
@@ -56,15 +62,17 @@ def getCurrentTrackID(sp):
     else:
         return current_playback['item']['uri'].split(":")[2]
 
+
 def getCurrentTrack(sp):
     try:
         track_id = getCurrentTrackID(sp)
         track_artists, track_name = getArtistsTitleForID(sp, track_id)
         track_meta = track_artists + "-" + track_name
-    except Exception:
+    except Exception("Nothing playing"):
         track_id = None
         track_meta = "Nothing"
     return track_id, track_meta
+
 
 def getArtistsTitleForID(sp, track_id):
     meta_track = sp.track(track_id)
@@ -76,20 +84,21 @@ def getArtistsTitleForID(sp, track_id):
             artists = artists + ', ' + artist['name']
     return artists, meta_track['name']
 
+
 def addTrackToPlaylist(rofi, rofi_args, sp, username, playlist_id, playlist_name, track_id, track_name):
     playlist_tracks_tmp = sp.playlist_tracks(playlist_id,  fields="total,items(track(id))",
                                              additional_types=('track',))
     playlist_tracks = playlist_tracks_tmp
     offset = 100
-    while(playlist_tracks_tmp['total'] == 100):
+    while offset < playlist_tracks_tmp['total']:
         playlist_tracks_tmp = sp.playlist_tracks(playlist_id, fields="total,items(track(id))", offset=offset,
                                                  additional_types=('track',))
         offset += 100
-        playlist_tracks.append(playlist_tracks_tmp)
+        playlist_tracks['items'].extend(playlist_tracks_tmp['items'])
     playlist_ids = [d['track']['id'] for d in playlist_tracks['items']]
     if track_id in playlist_ids:
-        index, key = rofi.select(track_name + " is already in " + playlist_name
-                                         + ". Add anyway? ", ["No", "Yes"], rofi_args=rofi_args)
+        index, key = rofi.select(track_name + " is already in " + playlist_name +
+                                 ". Add anyway? ", ["No", "Yes"], rofi_args=rofi_args)
         if index == -1:
             sys.exit(0)
         if index == 0:
@@ -98,6 +107,8 @@ def addTrackToPlaylist(rofi, rofi_args, sp, username, playlist_id, playlist_name
     rofi.status(track_name + " added to " + playlist_name + ".", rofi_args=rofi_args)
     time.sleep(2)
     rofi.close()
+    return results
+
 
 def run():
     config, config_dir = load_config()
@@ -106,7 +117,8 @@ def run():
     parser.add_argument("-a", "--add-to-playlist", action="store_true", help="Add current track to a playlist")
     parser.add_argument("-st", "--search-track", action="store_true", help="Search for a track")
     parser.add_argument('-i', '--case-sensitive', action='store_true', help='Enable case sensitivity')
-    parser.add_argument('-r', '--args', nargs=argparse.REMAINDER, help='Command line arguments for rofi. '                                                                   'Separate each argument with a space.')
+    parser.add_argument('-r', '--args', nargs=argparse.REMAINDER, help='Command line arguments for rofi. '
+                                                                       'Separate each argument with a space.')
     args = parser.parse_args()
 
     rofi_args = args.args or []
@@ -116,20 +128,18 @@ def run():
 
     scope = "user-library-read user-read-currently-playing user-read-playback-state user-library-modify " \
             "user-modify-playback-state playlist-modify-private playlist-read-private playlist-modify-public"
-    token = util.prompt_for_user_token(config['spotify']['spotify_username'],
-                                       scope=scope,
-                                       client_id=config['spotipy']['client_id'],
-                                       client_secret=config['spotipy']['client_secret'],
-                                       redirect_uri=config['spotipy']['redirect_uri'],
-                                       cache_path=(config_dir + "/token"))
-    sp = spotipy.Spotify(token)
+    sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(client_id=config['spotipy']['client_id'],
+                                                   client_secret=config['spotipy']['client_secret'],
+                                                   redirect_uri=config['spotipy']['redirect_uri'],
+                                                   scope=scope,
+                                                   cache_path=(config_dir + "/token")))
 
     if args.add_to_playlist:
         track_id, track_meta = getCurrentTrack(sp)
         playlists = getPlaylists(sp)
         playlists_names = [d['name'] for d in playlists['items']]
         index, key = rofi.select("To which playlist do you want to add " + track_meta + "? ",
-                                playlists_names, rofi_args=rofi_args)
+                                 playlists_names, rofi_args=rofi_args)
         if key == -1:
             sys.exit(0)
         target_playlist_id = playlists['items'][index]['id']
@@ -168,14 +178,17 @@ def run():
                 playlists = getPlaylists(sp)
                 playlists_names = [d['name'] for d in playlists['items']]
                 index_playlist, key_playlist = rofi.select("To which playlist do you want to add "
-                                                           + rofi_tracks[index_track] + "? ", playlists_names, rofi_args=rofi_args)
+                                                           + rofi_tracks[index_track] + "? ", playlists_names,
+                                                           rofi_args=rofi_args)
                 if key_playlist == -1:
                     sys.exit(0)
                 target_playlist_id = playlists['items'][index_playlist]['id']
-                result = addTrackToPlaylist(rofi, rofi_args, sp, config['spotify']['spotify_username'], target_playlist_id,
-                                   playlists_names[index_playlist], tracks[index_track]['id'], rofi_tracks[index_track])
+                result = addTrackToPlaylist(rofi, rofi_args, sp, config['spotify']['spotify_username'],
+                                            target_playlist_id, playlists_names[index_playlist],
+                                            tracks[index_track]['id'], rofi_tracks[index_track])
                 if not result == 0:
-                    rofi.status(rofi_tracks[index_track] + " added to " + playlists_names[index_playlist] + ".", rofi_args=rofi_args)
+                    rofi.status(rofi_tracks[index_track] + " added to " + playlists_names[index_playlist] + ".",
+                                rofi_args=rofi_args)
                     time.sleep(2)
                     rofi.close()
 
@@ -199,3 +212,4 @@ def run():
         rofi_args.append('-st')
         subprocess.run(["rofi-spotify", ', '.join(rofi_args)])
     sys.exit(0)
+run()
